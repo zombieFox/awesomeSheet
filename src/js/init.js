88369f10d93b7d405055acd4cdfa380341ba34b7
Kuldeep <crudetoaster@gmail.com>
2016-03-03 19:50:53 +0000

    [refactor] add new character js

--- src/js/init.js
+++ src/js/init.js
@@ -1,24 +1,14 @@
 (function() {
 
-  // bind nav controls
   nav.bind();
-  // save current character object to local storage
   sheet.storeCharacter();
-  // bind all inputs
   inputBlock.bind(helper.eA(".input-block"));
-  // fill in all inputs
   inputBlock.render();
-  // bind all teaxtareas
   textareaBlock.bind(helper.eA(".textarea-block"));
-  // fill in all textareas
   textareaBlock.render();
-  // render spells
   spells.render();
-  // render stat mods
   stats.render();
-  // bind stats
   stats.bind();
-  // sum all total blocks
-  totalBlock.total();
+  totalBlock.render();
 
 })();
