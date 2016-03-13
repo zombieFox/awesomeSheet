300243ea1b8e4bfb1b440208b4c6735ca6c75afa
Kuldeep Matharu <crudetoaster@gmail.com>
2016-03-13 22:25:00 +0000

    [design] UI fixes

--- build/js/awesomeSheet.js
+++ build/js/awesomeSheet.js
@@ -1475,9 +1475,9 @@ var clone = (function() {
       '</div>' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-12">' +
+      '<div class="col-xs-4 col-xs-offset-4">' +
       '<div class="clone-delete-controls">' +
-      '<button class="button button-primary button-small" tabindex="3"><span class="icon-close"></span></button>' +
+      '<button class="button button-primary button-small button-block" tabindex="3"><span class="icon-close"></span></button>' +
       '</div>' +
       '</div>' +
       '</div>';
@@ -1493,39 +1493,39 @@ var clone = (function() {
       '<input class="input-field input-weapon" id="input-weapon-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-2 col-md-2">' +
+      '<div class="col-xs-3 col-md-2">' +
       '<div class="input-block">' +
       '<label class="input-label" for="input-attack-' + index + '">Attack</label>' +
       '<input class="input-field input-attack" id="input-attack-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-2 col-md-2">' +
+      '<div class="col-xs-3 col-md-2">' +
       '<div class="input-block">' +
       '<label class="input-label" for="input-damage-' + index + '">Damage</label>' +
       '<input class="input-field input-damage" id="input-damage-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-2 col-md-2">' +
+      '<div class="col-xs-4 col-md-2">' +
       '<div class="input-block">' +
       '<label class="input-label" for="input-critical-' + index + '">Critical</label>' +
       '<input class="input-field input-critical" id="input-critical-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-3 col-md-1">' +
+      '<div class="col-xs-4 col-md-1">' +
       '<div class="input-block">' +
       '<label class="input-label" for="input-range-' + index + '">Range</label>' +
       '<input class="input-field input-range" id="input-range-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-3 col-md-1">' +
+      '<div class="col-xs-4 col-md-1">' +
       '<div class="input-block">' +
       '<label class="input-label" for="input-ammo-' + index + '">Ammo</label>' +
       '<input class="input-field input-ammo" id="input-ammo-' + index + '" type="text" tabindex="3">' +
       '</div>' +
       '</div>' +
-      '<div class="col-xs-6 col-md-12">' +
+      '<div class="col-xs-4 col-xs-offset-4">' +
       '<div class="clone-delete-controls">' +
-      '<button class="button button-primary button-small" id="remove-attack" tabindex="3"><span class="icon-close"></span></button>' +
+      '<button class="button button-primary button-small button-block" id="remove-attack" tabindex="3"><span class="icon-close"></span></button>' +
       '</div>' +
       '</div>' +
       '</div>';
