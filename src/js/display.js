e1ee38ffe4253512173c850753023ef0803f5c07
Kuldeep Matharu <crudetoaster@gmail.com>
2016-05-05 01:43:39 +0100

    [feature] display mode added

--- src/js/display.js
+++ src/js/display.js
@@ -34,31 +34,40 @@ var display = (function() {
   // };
 
   function render() {
-    var all_displayBlock = helper.eA(".js-display-block");
 
-    for (var i = 0; i < all_displayBlock.length; i++) {
-
-      var all_displayItem = all_displayBlock[i].querySelectorAll(".js-display-item");
-
-      for (var j = 0; j < all_displayItem.length; j++) {
-        var path = all_displayItem[j].dataset.path;
-        var target = all_displayItem[j].querySelector(".js-display-target");
-
-        if (path) {
-          var content = helper.getObject(sheet.getCharacter(), path);
-          if (typeof content != "undefined" && content != "") {
-            target.textContent = content;
-            helper.removeClass(all_displayItem[j], "is-hidden");
-          } else {
-            target.textContent = "";
-            helper.addClass(all_displayItem[j], "is-hidden");
-          };
+    var all_displayItem = helper.eA(".js-display-item");
+    for (var i = 0; i < all_displayItem.length; i++) {
+      var path = all_displayItem[i].dataset.path;
+      var target = all_displayItem[i].querySelector(".js-display-target");
+      if (path) {
+        var content = helper.getObject(sheet.getCharacter(), path);
+        if (typeof content != "undefined" && content != "") {
+          target.textContent = content;
+          helper.removeClass(all_displayItem[i], "is-hidden");
+        } else {
+          target.textContent = "";
+          helper.addClass(all_displayItem[i], "is-hidden");
         };
-
       };
-
-
     };
+
+    var all_displayTotal = helper.eA(".js-display-total");
+    for (var i = 0; i < all_displayTotal.length; i++) {
+      var source = all_displayTotal[i].dataset.source;
+      var target = all_displayTotal[i].querySelector(".js-display-target");
+      if (source) {
+        var content = helper.e("." + source).innerHTML;
+        if (typeof content != "undefined" && content != "") {
+          target.textContent = content;
+          helper.removeClass(all_displayTotal[i], "is-hidden");
+        } else {
+          target.textContent = "";
+          helper.addClass(all_displayTotal[i], "is-hidden");
+        };
+      };
+    };
+
+
   };
 
   // if (typeof content == "undefined" || content == "") {
