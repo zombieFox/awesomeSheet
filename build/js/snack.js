68269a427048989b2ba0e43e1ec6c27d08240dcb
Kuldeep Matharu <crudetoaster@gmail.com>
2016-05-07 15:15:28 +0100

    [bug] fix snack bar not being cleaned up after new snack is rendered

--- build/js/snack.js
+++ build/js/snack.js
@@ -3,34 +3,21 @@ var snack = (function() {
   var previousSnackBar = null;
 
   function destroy() {
-    var snackBar = helper.e(".js-snack-bar");
-    if (snackBar) {
-      getComputedStyle(snackBar).opacity;
-      snackBar.style.opacity = 0;
+    var all_snackBar = helper.eA(".js-snack-bar");
+    for (var i = 0; i < all_snackBar.length; i++) {
+      all_snackBar[i].destroy();
     };
   };
 
   function render(message, actionText) {
 
-    if (previousSnackBar) {
-      previousSnackBar.destroy();
-    };
+    var body = helper.e("body");
 
     var snackBar = document.createElement("aside");
     snackBar.setAttribute("class", "m-snack-bar js-snack-bar");
     snackBar.destroy = function() {
-      this.style.opacity = 0;
+      helper.addClass(snackBar, "is-transparent");
     };
-
-    snackBar.addEventListener("transitionend", function(event, elapsed) {
-      if (event.propertyName === "opacity" && this.style.opacity == 0) {
-        this.parentElement.removeChild(this);
-        if (previousSnackBar === this) {
-          previousSnackBar = null;
-        };
-      };
-    }.bind(snackBar), false);
-
     var text = document.createElement("p");
     text.className = "m-snack-bar-message";
     text.textContent = (message);
@@ -45,25 +32,29 @@ var snack = (function() {
       snackBar.appendChild(actionButton);
     };
 
+    snackBar.addEventListener("transitionend", function(event, elapsed) {
+      if (event.propertyName === "opacity" && this.style.opacity == 0) {
+        this.parentElement.removeChild(this);
+      };
+    }.bind(snackBar), false);
+
+    if (previousSnackBar) {
+      previousSnackBar.destroy();
+    };
+
+    previousSnackBar = snackBar;
+
     setTimeout(function() {
       if (previousSnackBar === this) {
         previousSnackBar.destroy();
       };
-    }.bind(snackBar), 5000);
+    }.bind(snackBar), 111114000);
 
-    previousSnackBar = snackBar;
-
-    document.body.appendChild(snackBar);
-    // In order for the animations to trigger, I have to force the original style to be computed, and then change it.
+    body.appendChild(snackBar);
     getComputedStyle(snackBar).opacity;
-    snackBar.style.opacity = 1;
+    getComputedStyle(snackBar).transform;
+    getComputedStyle(snackBar).margin;
     helper.addClass(snackBar, "is-reveal");
-
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode == 27) {
-        destroy();
-      };
-    }, false);
 
   };
 
