06a1da623c695cd46a1db9ee50cd2096eb037141
Kuldeep M <crudetoaster@gmail.com>
2017-05-31 09:38:17 +0100

    [design] UI fixes and improvements

--- build/js/nav.js
+++ build/js/nav.js
@@ -134,7 +134,7 @@ var nav = (function() {
         var pinWatch = helper.e("." + all_editControls[i].dataset.pinWatch);
         var fillWidth = parseInt(getComputedStyle(all_editControls[i]).width, 10);
         var fillHeight = parseInt(getComputedStyle(all_editControls[i]).height, 10) + parseInt(getComputedStyle(all_editControls[i]).marginTop, 10) + parseInt(getComputedStyle(all_editControls[i]).marginBottom, 10);
-        if (pinWatch.getBoundingClientRect().top <= (offset - fillHeight) && pinWatch.getBoundingClientRect().bottom >= offset) {
+        if (pinWatch.getBoundingClientRect().top <= (offset - fillHeight) && pinWatch.getBoundingClientRect().bottom >= (offset + fillHeight)) {
           helper.addClass(pinWatch, "is-pinned");
           if (!pinWatch.hasAttribute("style")) {
             all_editControls[i].setAttribute("style", "width: " + fillWidth + "px");
