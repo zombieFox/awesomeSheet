806c73e46a1a3b267e78131a88ce4fd8d8f6a7ba
Kuldeep Matharu <crudetoaster@gmail.com>
2017-06-01 23:34:12 +0100

    [refactor] clean up fireball

--- src/js/fireball.js
+++ src/js/fireball.js
@@ -6,10 +6,6 @@ var fireball = (function() {
     var fireball = document.createElement("div");
     fireball.setAttribute("class", "m-fireball js-fireball");
 
-    fireball.destroy = function() {
-      helper.addClass(fireball, "is-transparent");
-    };
-
     fireball.addEventListener("animationend", function(event, elapsed) {
       this.parentElement.removeChild(this);
     }.bind(fireball), false);
