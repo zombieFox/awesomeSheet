293d3be9b3f544b4bb8986c4b3a16991706e1bea
Kuldeep Matharu <crudetoaster@gmail.com>
2017-06-03 18:56:23 +0100

    [refactor] improve floating edit control detection

--- build/js/awesomeSheet.js
+++ build/js/awesomeSheet.js
@@ -12010,18 +12010,22 @@ var nav = (function() {
 
       for (var i = 0; i < all_editControls.length; i++) {
         var pinWatch = helper.e("." + all_editControls[i].dataset.pinWatch);
+        var section = helper.getClosest(pinWatch, ".js-section");
         var fillWidth = parseInt(getComputedStyle(all_editControls[i]).width, 10);
         var fillHeight = parseInt(getComputedStyle(all_editControls[i]).height, 10) + parseInt(getComputedStyle(all_editControls[i]).marginTop, 10) + parseInt(getComputedStyle(all_editControls[i]).marginBottom, 10);
-        if (pinWatch.getBoundingClientRect().top <= (offset - fillHeight) && pinWatch.getBoundingClientRect().bottom >= (offset + fillHeight)) {
-          helper.addClass(pinWatch, "is-pinned");
-          if (!pinWatch.hasAttribute("style")) {
-            all_editControls[i].setAttribute("style", "width: " + fillWidth + "px");
-            pinWatch.setAttribute("style", "padding-top: " + fillHeight + "px");
+        if (section.dataset.minimise == "false" || !section.dataset.minimise && section.dataset.displayMode == "false" || !section.dataset.displayMode) {
+          if (pinWatch.getBoundingClientRect().top <= (offset - fillHeight) && pinWatch.getBoundingClientRect().bottom >= (offset + fillHeight)) {
+            // console.log("fire", pinWatch);
+            helper.addClass(pinWatch, "is-pinned");
+            if (!pinWatch.hasAttribute("style")) {
+              all_editControls[i].setAttribute("style", "width: " + fillWidth + "px");
+              pinWatch.setAttribute("style", "padding-top: " + fillHeight + "px");
+            };
+          } else {
+            helper.removeClass(pinWatch, "is-pinned");
+            pinWatch.removeAttribute("style");
+            all_editControls[i].removeAttribute("style");
           };
-        } else {
-          helper.removeClass(pinWatch, "is-pinned");
-          pinWatch.removeAttribute("style");
-          all_editControls[i].removeAttribute("style");
         };
       };
 
