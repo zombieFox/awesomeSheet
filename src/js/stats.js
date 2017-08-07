692f4fc22d952a16845507bad5eb1034fac78589
Kuldeep M <crudetoaster@gmail.com>
2017-08-07 14:26:53 +0100

    [feature] adding automatic encumbrance

--- src/js/stats.js
+++ src/js/stats.js
@@ -23,7 +23,7 @@ var stats = (function() {
     helper.setObject(sheet.getCharacter(), path, modifier);
     // add a + if greater than 0
     if (modifier > 0) {
-      modifier =  "+" + modifier
+      modifier = "+" + modifier
     };
     // render modifier
     totalElement.textContent = modifier;
@@ -38,6 +38,7 @@ var stats = (function() {
   };
 
   var changeModiferTimer = null;
+  var changeEncumbranceTimer = null;
 
   function delayUpdate(element) {
     _render_stat(element);
@@ -50,7 +51,47 @@ var stats = (function() {
     };
   };
 
+  function delayUpdateEncumbrance() {
+    console.log(_encumbrance(get_score("str")));
+  };
+
   function bind() {
+    _bind_all_stats();
+    _bind_str();
+  };
+
+  function _encumbrance(str) {
+    var allCapacity = [];
+    var lightLoad;
+    var mediumLoad;
+    var heavyLoad;
+    var base = [25, 28.75, 32.5, 37.5, 43.75, 50, 57.5, 65, 75, 87.5];
+    if (str <= 10) {
+      heavyLoad = 10 * str;
+    } else {
+      var index = (1 + str - 10 * parseInt(str / 10)) - 1;
+      heavyLoad = base[index] * Math.pow(4, parseInt(str / 10));
+    };
+    allCapacity.push(parseInt(heavyLoad / 3) + " lbs. or less");
+    allCapacity.push(parseInt(heavyLoad/3) + 1 + " - " + parseInt((2 * heavyLoad) / 3) + " lbs.");
+    allCapacity.push(parseInt((2 * heavyLoad) / 3) + 1 + " - " + heavyLoad + " lbs.");
+    return allCapacity;
+  };
+
+  function _bind_str() {
+    var statsStrScore = helper.e("#statistics-stats-str-score");
+    var statsStrTempScore = helper.e("#statistics-stats-str-temp-score");
+    statsStrScore.addEventListener("input", function() {
+      clearTimeout(changeEncumbranceTimer);
+      changeEncumbranceTimer = setTimeout(delayUpdateEncumbrance, 350);
+    }, false);
+    statsStrTempScore.addEventListener("input", function() {
+      clearTimeout(changeEncumbranceTimer);
+      changeEncumbranceTimer = setTimeout(delayUpdateEncumbrance, 350);
+    }, false);
+  };
+
+  function _bind_all_stats() {
     var score = helper.eA(".js-stats-score");
     var tempScore = helper.eA(".js-stats-temp-score");
     for (var i = 0; i < score.length; i++) {
@@ -77,11 +118,22 @@ var stats = (function() {
     return value;
   };
 
+  function get_score(key) {
+    var value = 0;
+    if (sheet.getCharacter().statistics.stats[key].temp_score != "") {
+      value = sheet.getCharacter().statistics.stats[key].temp_score;
+    } else {
+      value = sheet.getCharacter().statistics.stats[key].score;
+    };
+    return value;
+  };
+
   // exposed methods
   return {
     render: render,
     bind: bind,
-    getMod: get_mod
+    getMod: get_mod,
+    getScore: get_score,
   };
 
 })();
