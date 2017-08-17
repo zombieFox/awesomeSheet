7712b43291a82a9a0bfdd89fb7ddde4fd6d4d1e4
Kuldeep M <crudetoaster@gmail.com>
2017-08-17 12:40:15 +0100

    [feature] adding section tabs

--- src/js/display.js
+++ src/js/display.js
@@ -69,11 +69,13 @@ var display = (function() {
     var icon = element.querySelector(".js-card-toggle-icon");
     var section = helper.getClosest(element, ".js-section");
     var edit = section.querySelector(".js-edit");
+    var editTabs = section.querySelector(".js-edit-tabs");
     var all_display = section.querySelectorAll(".js-display");
     var _displayOn = function() {
       section.dataset.displayMode = "true";
       helper.addClass(section, "is-display-mode");
       helper.addClass(edit, "is-hidden");
+      helper.addClass(editTabs, "is-hidden");
       for (var i = 0; i < all_display.length; i++) {
         helper.removeClass(all_display[i], "is-hidden");
       };
@@ -84,6 +86,7 @@ var display = (function() {
       section.dataset.displayMode = "false";
       helper.removeClass(section, "is-display-mode");
       helper.removeClass(edit, "is-hidden");
+      helper.removeClass(editTabs, "is-hidden");
       for (var i = 0; i < all_display.length; i++) {
         helper.addClass(all_display[i], "is-hidden");
       };
