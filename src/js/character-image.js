207763eb0f1ac3d399be788d084c1b3ab3d89b74
Kuldeep M <crudetoaster@gmail.com>
2017-11-24 10:43:45 +0000

    [feature] adding character image

--- src/js/character-image.js
+++ src/js/character-image.js
@@ -22,10 +22,10 @@ var characterImage = (function() {
       renderInputTimer = setTimeout(delayRender, 300, this);
     }, false);
     characterImageScaleDecrease.addEventListener("click", function() {
-      _resizeCharacterImage();
+      _resize();
     }, false);
     characterImageScaleIncrease.addEventListener("click", function() {
-      _resizeCharacterImage();
+      _resize();
     }, false);
     characterImageScaleCover.addEventListener("click", function() {
       _characterImageSizePreset("cover");
@@ -36,7 +36,7 @@ var characterImage = (function() {
   };
 
   function delayRender(element) {
-    _resizeCharacterImage();
+    _resize();
   };
 
   function _removeCharacterImage() {
@@ -61,9 +61,7 @@ var characterImage = (function() {
       tempImage.onload = function() {
         // check width and height
         if (tempImage.width <= 2000 || tempImage.height <= 2000) {
-          helper.setObject(sheet.getCharacter(), "basics.character_image.image", reader.result);
-          sheet.storeCharacters();
-          render();
+          _calculateCoverSize(reader.result);
         } else {
           snack.render("Image too large, max 2000x2000px.", false, false);
         };
@@ -81,6 +79,70 @@ var characterImage = (function() {
     } else {
       snack.render("File too big, max 500KB.", false, false);
     };
+  };
+
+  function _calculateCoverSize(imageBase64) {
+    var scale;
+    var characterImagePreview = helper.e(".js-character-image-preview");
+    var tempImage = new Image;
+    var size = {
+      w: 0,
+      h: 0,
+      bigW: 0,
+      bigH: 0
+    }
+    var landscapeOrPortrait;
+    tempImage.onload = function() {
+      console.log("onload fire");
+      size.w = this.width;
+      size.h = this.height;
+      size.bigW = characterImagePreview.getBoundingClientRect().width;
+      size.bigH = characterImagePreview.getBoundingClientRect().height;
+      if (size.w > size.h) {
+        landscapeOrPortrait = "landscape";
+      } else if (size.w == size.h) {
+        landscapeOrPortrait = "portrait";
+      } else {
+        landscapeOrPortrait = "square";
+      };
+      // console.log("preview: ", "\t\tW: " + size.bigW, "\t\tH: " + size.bigH);
+      // console.log("image: ", "\t\tw: " + size.w, "\t\t\th: " + size.h, "\t\t" + landscapeOrPortrait);
+      // console.log((size.bigH / ((size.bigW / size.w) * size.h)) * 100);
+      scale = (size.bigH / ((size.bigW / size.w) * size.h)) * 100;
+      _storeLoadedImage(imageBase64, scale);
+    };
+    tempImage.src = imageBase64;
+  };
+
+  function _storeLoadedImage(imageBase64, scale) {
+    console.log("store fire");
+    helper.setObject(sheet.getCharacter(), "basics.character_image.image", imageBase64);
+    helper.setObject(sheet.getCharacter(), "basics.character_image.scale", parseInt(scale, 10) + 1);
+    sheet.storeCharacters();
+    render();
+  };
+
+  function render() {
+    var characterImagePreview = helper.e(".js-character-image-preview");
+    var imageBase64 = helper.getObject(sheet.getCharacter(), "basics.character_image.image");
+    if (imageBase64) {
+      characterImagePreview.style.backgroundImage = "url(" + imageBase64 + ")";
+    };
+    _resize();
+  };
+
+  function _resize() {
+    console.log("resize");
+    var characterImagePreview = helper.e(".js-character-image-preview");
+    var scale = helper.getObject(sheet.getCharacter(), "basics.character_image.scale");
+    characterImagePreview.style.backgroundSize = scale + "%";
+    // if (value && !isNaN(value)) {
+    //   characterImagePreview.style.backgroundSize = value + "%";
+    // } else if (value && typeof value == "string" && value != "") {
+    //   characterImagePreview.style.backgroundSize = value;
+    // } else {
+    //   characterImagePreview.style.backgroundSize = "cover";
+    // };
   };
 
   function _clearInput(input) {
@@ -108,58 +170,6 @@ var characterImage = (function() {
     helper.setObject(sheet.getCharacter(), "basics.character_image.scale", value);
     characterImagePreview.style.backgroundSize = value;
     sheet.storeCharacters();
-  };
-
-  function render() {
-    var characterImagePreview = helper.e(".js-character-image-preview");
-    var imageBase64 = helper.getObject(sheet.getCharacter(), "basics.character_image.image");
-    if (imageBase64) {
-      characterImagePreview.style.backgroundImage = "url(" + imageBase64 + ")";
-    };
-    _resizeCharacterImage();
-  };
-
-  function _resizeCharacterImage() {
-    _calculateCoverSize();
-    var characterImagePreview = helper.e(".js-character-image-preview");
-    var value = helper.getObject(sheet.getCharacter(), "basics.character_image.scale");
-    if (value && !isNaN(value)) {
-      characterImagePreview.style.backgroundSize = value + "%";
-    } else if (value && typeof value == "string" && value != "") {
-      characterImagePreview.style.backgroundSize = value;
-    } else {
-      characterImagePreview.style.backgroundSize = "cover";
-    };
-  };
-
-  function _calculateCoverSize() {
-    var imageBase64 = helper.getObject(sheet.getCharacter(), "basics.character_image.image");
-    var characterImagePreview = helper.e(".js-character-image-preview");
-    var tempImage = new Image;
-    var size = {
-      w: 0,
-      h: 0,
-      bigW: 0,
-      bigH: 0
-    }
-    var landscapeOrPortrait;
-    tempImage.onload = function() {
-      size.w = this.width;
-      size.h = this.height;
-      size.bigW = characterImagePreview.getBoundingClientRect().width;
-      size.bigH = characterImagePreview.getBoundingClientRect().height;
-      if (size.w > size.h) {
-        landscapeOrPortrait = "landscape";
-      } else if (size.w == size.h) {
-        landscapeOrPortrait = "portrait";
-      } else {
-        landscapeOrPortrait = "square";
-      };
-      console.log("preview: ", "\t\tW: " + size.bigW, "\t\tH: " + size.bigH);
-      console.log("image: ", "\t\tw: " + size.w, "\t\t\th: " + size.h, "\t\t" + landscapeOrPortrait);
-      console.log((size.bigH / ((size.bigW / size.w) * size.h)) * 100);
-    };
-    tempImage.src = imageBase64;
   };
 
   // exposed methods
