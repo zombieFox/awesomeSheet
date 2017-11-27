7ec7c2a62d6fe6b56887954d1b23a939cf60c305
Kuldeep M <crudetoaster@gmail.com>
2017-11-27 12:49:37 +0000

    [feature] adding character image

--- src/js/helper.js
+++ src/js/helper.js
@@ -259,6 +259,50 @@ var helper = (function() {
     return object;
   };
 
+  function getAverageColor(imageBase64) {
+    // var imageUrl = elementWithBackgroundImage.style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    var tempImage = new Image;
+    tempImage.src = imageBase64;
+    var blockSize = 5, // only visit every 5 pixels
+      defaultRGB = {
+        r: 0,
+        g: 0,
+        b: 0
+      }, // for non-supporting envs
+      canvas = document.createElement('canvas'),
+      context = canvas.getContext && canvas.getContext('2d'),
+      data, width, height,
+      i = -4,
+      length,
+      rgb = {
+        r: 0,
+        g: 0,
+        b: 0
+      },
+      count = 0;
+    if (!context) {
+      return defaultRGB;
+    };
+    canvas.height = tempImage.naturalHeight || tempImage.offsetHeight || tempImage.height;
+    canvas.width = tempImage.naturalWidth || tempImage.offsetWidth || tempImage.width;
+    height = canvas.height;
+    width = canvas.width;
+    context.drawImage(tempImage, 0, 0);
+    data = context.getImageData(0, 0, width, height);
+    length = data.data.length;
+    while ((i += blockSize * 4) < length) {
+      ++count;
+      rgb.r += data.data[i];
+      rgb.g += data.data[i + 1];
+      rgb.b += data.data[i + 2];
+    };
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r / count);
+    rgb.g = ~~(rgb.g / count);
+    rgb.b = ~~(rgb.b / count);
+    return rgb;
+  };
+
   // exposed methods
   return {
     store: store,
@@ -284,7 +328,8 @@ var helper = (function() {
     pasteStrip: pasteStrip,
     inViewport: inViewport,
     sortObject: sortObject,
-    getDateTime: getDateTime
+    getDateTime: getDateTime,
+    getAverageColor: getAverageColor
   };
 
 })();
