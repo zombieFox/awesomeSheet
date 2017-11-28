b8422accd864c5863afd25c618f4740cf29b2825
Kuldeep Matharu <crudetoaster@gmail.com>
2017-11-28 23:22:15 +0000

    [feature] improving how stats are controlled

--- src/js/repair.js
+++ src/js/repair.js
@@ -4,16 +4,16 @@ var repair = (function() {
     // console.log("fire repair update");
     // update stats
     if (!characterObject.statistics.stats.str.enhancement && characterObject.statistics.stats.str.enhancement != "" || !characterObject.statistics.stats.dex.enhancement && characterObject.statistics.stats.dex.enhancement != "" || !characterObject.statistics.stats.con.enhancement && characterObject.statistics.stats.con.enhancement != "" || !characterObject.statistics.stats.int.enhancement && characterObject.statistics.stats.int.enhancement != "" || !characterObject.statistics.stats.wis.enhancement && characterObject.statistics.stats.wis.enhancement != "" || !characterObject.statistics.stats.cha.enhancement && characterObject.statistics.stats.cha.enhancement != "") {
-      // console.log(characterObject.basics.name);
+      console.log(characterObject.basics.name);
       for (var key in characterObject.statistics.stats) {
         var score = parseInt(characterObject.statistics.stats[key].score, 10);
-        // console.log(key, characterObject.statistics.stats[key].score, "score", score);
         var tempScore = parseInt(characterObject.statistics.stats[key].temp_score, 10);
-        var newTempScore;
+        console.log(key, "new score = ", score, "\t\ttempScore = ", tempScore);
         characterObject.statistics.stats[key].base = score;
         if (!isNaN(tempScore)) {
-          newTempScore = tempScore - score;
-          characterObject.statistics.stats[key].temp = newTempScore;
+          characterObject.statistics.stats[key].temp = (tempScore - score);
+        } else {
+          characterObject.statistics.stats[key].temp = "";
         };
         delete characterObject.statistics.stats[key].score;
         delete characterObject.statistics.stats[key].temp_score;
