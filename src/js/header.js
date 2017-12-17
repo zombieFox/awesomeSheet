31f091be1d075cf818bb20a84dcdfcb79f7d7403
Kuldeep Matharu <crudetoaster@gmail.com>
2017-12-16 22:05:21 -0600

    [feature] new header

--- src/js/header.js
+++ src/js/header.js
@@ -1,24 +1,32 @@
 var header = (function() {
 
   var previousPosition = window.pageYOffset;
+  var target = null;
 
   function scroll() {
     var body = helper.e("body");
     var header = helper.e(".js-header");
     var nav = helper.e(".js-nav");
     var currentPosition = window.pageYOffset;
-    if (currentPosition > (window.innerHeight / 8)) {
-      if (previousPosition > currentPosition) {
-        helper.removeClass(header, "is-up");
-        helper.removeClass(nav, "is-up");
-        body.dataset.headerUp = false;
-      } else {
-        helper.addClass(header, "is-up");
-        helper.addClass(nav, "is-up");
-        body.dataset.headerUp = true;
+    if (previousPosition > currentPosition) {
+      helper.removeClass(header, "is-pinned");
+      helper.removeClass(nav, "is-pinned");
+      body.dataset.headerPinned = false;
+      target = null;
+    } else {
+      if (target == null) {
+        if (body.dataset.headerPinned == "false" || !body.dataset.headerPinned) {
+          target = window.pageYOffset + 199;
+        };
+      } else if (currentPosition == target || currentPosition >= target) {
+        helper.addClass(header, "is-pinned");
+        helper.addClass(nav, "is-pinned");
+        body.dataset.headerPinned = true;
+        target = null;
       };
-      previousPosition = currentPosition;
     };
+    previousPosition = currentPosition;
+    // console.log("previous", previousPosition, "target", target);
   };
 
   // exposed methods
