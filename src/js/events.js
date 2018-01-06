8d554e1abbee6a3c4cf9e4c5f4e8831de73caef5
Kuldeep Matharu <crudetoaster@gmail.com>
2018-01-06 13:41:35 +0000

    [refactor] improve events logging

--- src/js/events.js
+++ src/js/events.js
@@ -1,18 +1,14 @@
 var events = (function() {
 
   function bind() {
-    var eventXp = helper.e(".js-evets-xp");
-    var eventWealth = helper.e(".js-evets-wealth");
-    eventXp.addEventListener("click", function(event) {
-      event.stopPropagation();
-      event.preventDefault();
-      render("xp");
-    }, false)
-    eventWealth.addEventListener("click", function(event) {
-      event.stopPropagation();
-      event.preventDefault();
-      render("wealth");
-    }, false)
+    var all_eventLogs = helper.eA(".js-evets-log");
+    for (var i = 0; i < all_eventLogs.length; i++) {
+      all_eventLogs[i].addEventListener("click", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        render(this);
+      }, false)
+    };
   };
 
   function _create_event(type, eventObject) {
@@ -48,105 +44,93 @@ var events = (function() {
     return timestampString;
   };
 
-  function _create_eventTr(eventLogType, eventObject) {
-    // console.log(eventLogType, eventObject);
-    var tr = document.createElement("tr");
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var para = document.createElement("p");
-    var data;
-    if ("aggregate_value" in eventObject.event) {
-      data = eventObject.event.aggregate_value.toLocaleString(undefined, {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0
-      });
-      if (eventObject.event.aggregate_value > 0) {
-        data = "+" + data;
-      };
-      if (eventLogType == "xp") {
-        data = data + " XP";
-      } else if (eventLogType == "wealth") {
-        if (eventObject.type == "platinum") {
-          data = data + " PP";
-        } else if (eventObject.type == "gold") {
-          data = data + " GP";
-        } else if (eventObject.type == "silver") {
-          data = data + " SP";
-        } else if (eventObject.type == "copper") {
-          data = data + " CP";
+  function render(button) {
+    var options = helper.makeObject(button.dataset.eventsOptions);
+    var _create_eventTr = function(eventObject) {
+      // console.log(eventLogType, eventObject);
+      var tr = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      var para = document.createElement("p");
+      var data;
+      if ("aggregate_value" in eventObject.event) {
+        data = eventObject.event.aggregate_value.toLocaleString(undefined, {
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0
+        });
+        if (eventObject.event.aggregate_value > 0) {
+          data = "+" + data;
         };
+        if (options.type == "xp") {
+          data = data + " XP";
+        } else if (options.type == "wealth") {
+          if (eventObject.type == "platinum") {
+            data = data + " PP";
+          } else if (eventObject.type == "gold") {
+            data = data + " GP";
+          } else if (eventObject.type == "silver") {
+            data = data + " SP";
+          } else if (eventObject.type == "copper") {
+            data = data + " CP";
+          };
+        };
+      } else if ("note" in eventObject.event) {
+        data = eventObject.event.note;
       };
-    } else if ("note" in eventObject.event) {
-      data = eventObject.event.note;
+      para.textContent = data;
+      var timestamp = document.createElement("p");
+      timestamp.setAttribute("class", "u-small-text u-text-right");
+      timestamp.textContent = _timestampString(eventObject.timestamp);
+      td2.appendChild(para);
+      td1.appendChild(timestamp);
+      tr.appendChild(td2);
+      tr.appendChild(td1);
+      return tr;
     };
-    para.textContent = data;
-    var timestamp = document.createElement("p");
-    timestamp.setAttribute("class", "u-small-text u-text-right");
-    timestamp.textContent = _timestampString(eventObject.timestamp);
-    td2.appendChild(para);
-    td1.appendChild(timestamp);
-    tr.appendChild(td2);
-    tr.appendChild(td1);
-    return tr;
-  };
-
-  function _create_eventTable(eventLogType) {
-    var table = document.createElement("table");
-    var tbody = document.createElement("tbody");
-    var all_events = helper.getObject({
-      object: sheet.getCharacter(),
-      path: "events"
-    });
-    var all_eventsToRender = [];
-    if (eventLogType == "xp") {
-      all_events.forEach(function(object) {
-        if (object.type == "xp") {
-          all_eventsToRender.push(object);
-        };
-      });
-    } else if (eventLogType == "wealth") {
-      all_events.forEach(function(object) {
-        if (object.type == "platinum" || object.type == "gold" || object.type == "silver" || object.type == "copper") {
-          all_eventsToRender.push(object);
-        };
-      });
-    };
-    // console.log("all_eventsToRender", all_eventsToRender);
-    if (all_eventsToRender.length > 0) {
-      for (var i in all_eventsToRender) {
-        var tr = _create_eventTr(eventLogType, all_eventsToRender[i]);
-        tbody.appendChild(tr);
-      };
-    } else {
+    var _create_eventTable = function() {
       var table = document.createElement("table");
       var tbody = document.createElement("tbody");
-      var tr = document.createElement("tr");
-      var td = document.createElement("td");
-      var message = document.createElement("p");
-      if (eventLogType == "xp") {
-        message.textContent = "No XP logged yet. Why not add some?";
-      } else if (eventLogType == "wealth") {
-        message.textContent = "No wealth logged yet. Why not add some?";
+      var all_events = helper.getObject({
+        object: sheet.getCharacter(),
+        path: "events"
+      });
+      var all_eventsToRender = [];
+      if (options.type == "xp") {
+        all_events.forEach(function(object) {
+          if (object.type == "xp") {
+            all_eventsToRender.push(object);
+          };
+        });
+      } else if (options.type == "wealth") {
+        all_events.forEach(function(object) {
+          if (object.type == "platinum" || object.type == "gold" || object.type == "silver" || object.type == "copper") {
+            all_eventsToRender.push(object);
+          };
+        });
       };
-      td.appendChild(message);
-      tr.appendChild(td);
-      tbody.appendChild(tr);
+      // console.log("all_eventsToRender", all_eventsToRender);
+      if (all_eventsToRender.length > 0) {
+        for (var i in all_eventsToRender) {
+          tbody.appendChild(_create_eventTr(all_eventsToRender[i]));
+        };
+      } else {
+        var table = document.createElement("table");
+        var tbody = document.createElement("tbody");
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var para = document.createElement("p");
+        para.textContent = options.emptyMessage;
+        td.appendChild(para);
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+      };
+      table.appendChild(tbody);
+      return table;
     };
-    table.appendChild(tbody);
-    return table;
-  };
-
-  function render(eventLogType) {
-    var heading;
-    if (eventLogType == "xp") {
-      heading = "XP log";
-    } else if (eventLogType == "wealth") {
-      heading = "Wealth log";
-    };
-    var body = _create_eventTable(eventLogType);
+    var modalBody = _create_eventTable();
     modal.render({
-      heading: heading,
-      content: body,
+      heading: options.modalHeading,
+      content: modalBody,
       actionText: "Close",
       size: "small"
     });
