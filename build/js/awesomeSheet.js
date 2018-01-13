ac51af86ebf803dbd9aa8b06ceb053aac1fb3e70
Kuldeep Matharu <crudetoaster@gmail.com>
2018-01-13 00:13:54 +0000

    [release] update build

--- build/js/awesomeSheet.js
+++ build/js/awesomeSheet.js
@@ -24764,6 +24764,7 @@ var xp = (function() {
     var trackSlow = [0, 3000, 7500, 14000, 23000, 35000, 53000, 77000, 115000, 160000, 235000, 330000, 475000, 665000, 955000, 1350000, 1900000, 2700000, 3850000, 5350000];
     var trackMedium = [0, 2000, 5000, 9000, 15000, 23000, 35000, 51000, 75000, 105000, 155000, 220000, 315000, 445000, 635000, 890000, 1300000, 1800000, 2550000, 3600000];
     var trackFast = [0, 1300, 3300, 6000, 10000, 15000, 23000, 34000, 50000, 71000, 105000, 145000, 210000, 295000, 425000, 600000, 850000, 1200000, 1700000, 2400000];
+    var trackPathfinderSociety = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57];
     var selectedTrack = false;
     var speed = helper.getObject({
       object: sheet.getCharacter(),
@@ -24783,6 +24784,8 @@ var xp = (function() {
       selectedTrack = trackMedium;
     } else if (speed == "Fast") {
       selectedTrack = trackFast;
+    } else if (speed == "Pathfinder Society") {
+      selectedTrack = trackPathfinderSociety;
     };
     var _render_nextXp = function() {
       if (selectedTrack) {
