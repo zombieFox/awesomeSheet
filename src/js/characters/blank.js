bda52d938b3a01dbee7224dd6c360b52fe6030d4
Kuldeep M <crudetoaster@gmail.com>
2018-02-13 14:14:26 +0000

    [bug] fix blank characrer object to reflect recent change

--- src/js/characters/blank.js
+++ src/js/characters/blank.js
@@ -28,18 +28,16 @@ var blank = (function() {
         }
       },
       classes: {
-        all: {
-          all: [{
-            classname: "",
-            level: "",
-            hp: "",
-            fortitude: "",
-            reflex: "",
-            will: "",
-            ranks: "",
-            bab: ""
-          }]
-        }
+        all: [{
+          classname: "",
+          level: "",
+          hp: "",
+          fortitude: "",
+          reflex: "",
+          will: "",
+          ranks: "",
+          bab: ""
+        }]
       },
       experience: {
         level: "",
