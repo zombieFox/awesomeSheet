f6a432a3a0c3513021860d58677c1964664c5db7
Kuldeep M <crudetoaster@gmail.com>
2018-02-14 16:12:33 +0000

    [refactor] improve database get function

--- src/js/pill.js
+++ src/js/pill.js
@@ -1,11 +1,30 @@
 var pill = (function() {
 
+  function _create_pillObecjtState() {
+    return {
+      state: null
+    };
+  };
+
   function bind() {
 
   };
 
   function clear() {
 
+  };
+
+  function _create_spellButton(name) {
+    var spellButton = document.createElement("button");
+    spellButton.setAttribute("class", " button button-medium js-spell");
+    spellButton.setAttribute("type", "button");
+    spellButton.setAttribute("tabindex", "1");
+    if (_spellState.get(level) == "remove") {
+      helper.addClass(spellButton, "button-primary");
+    } else if (_spellState.get(level) == "prepare" || _spellState.get(level) == "unprepare" || _spellState.get(level) == "cast" || _spellState.get(level) == "active") {
+      helper.addClass(spellButton, "button-secondary");
+    };
+    return spellButton;
   };
 
   function render(pillBlock) {
@@ -20,13 +39,16 @@ var pill = (function() {
   };
 
   function _render_pillBlock(pillBlock) {
-    var options = helper.makeObject(pillBlock.dataset.pillBlockOptions);
-    var pillBlockArea = pillBlock.querySelector(".js-pill-block-area");
-    var all_pillObjects = helper.getObject({
-      object: sheet.get(),
-      path: options.path
-    });
-    console.log(all_pillObjects);
+    pillBlock.state = null;
+    console.log(pillBlock);
+    console.log(pillBlock.state);
+    // var options = helper.makeObject(pillBlock.dataset.pillBlockOptions);
+    // var pillBlockArea = pillBlock.querySelector(".js-pill-block-area");
+    // var all_pillObjects = helper.getObject({
+    //   object: sheet.get(),
+    //   path: options.path
+    // });
+    // console.log(all_pillObjects);
   };
 
   function add() {
